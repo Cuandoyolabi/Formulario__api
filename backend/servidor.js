dc0a8f001e414ebf5a047fc8845a76c6c1d895cd
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-05-07 12:34:32 -0600

    Se inicia el proyecto con html, css, script.js y un backend con express que funciona como servidor

---
+++ backend/servidor.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors()); // permite peticiones desde el frontend
+app.use(express.json()); // para parsear JSON
+
+app.post('/enviar', (req, res) => {
+  console.log(req.body); // Ver los datos enviados
+  res.json({ mensaje: 'Datos recibidos correctamente', datos: req.body });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor escuchando en http://localhost:3000');
+});
